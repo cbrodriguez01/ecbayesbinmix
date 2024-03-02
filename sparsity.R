e8e7cb62c3539a7bf824ad8d1083d9e25114b7a6
cbrodriguez01 <crodriguezcabrera@g.harvard.edu>
2024-03-01 23:59:48 -0500

    edited sparsity code

--- sparsity.R
+++ sparsity.R
@@ -1,14 +1,63 @@
 #Checking the sparsity of the Dirichlet prior on the mixing weights
 #https://stats.stackexchange.com/questions/239216/what-is-meant-by-non-convex-prior-and-sparsity-inducing-prior
 
-K<-10
-alpha<- c(1,0.5, 1/(seq(10,60, by = 5)))
 
-pd<-list()
-for (i in 1:length(alpha)){
-   alphavec<-rep(alpha[i], K)
-   pdir<- list(BayesBinMix::myDirichlet(alphavec))
-  pd<-append(pd, pdir)
-}
+#alpha<- c(1, 1/(seq(20,60, by = 15)))
+
+# pd<-list()
+# for (i in 1:length(alpha)){
+#    alphavec<-rep(alpha[i], K)
+#    pdir<- list(BayesBinMix::myDirichlet(alphavec))
+#   pd<-append(pd, pdir)
+# }
 
 #Note that as alpha decreases so does the number of vector elements with much appreciable mass, thus inducing sparsity
+
+library(BayesBinMix)
+library(DescTools)
+library(tidyverse)
+#We are going to explore changes in the value of K for the ACS 2010
+censusdata_bin <- readRDS("./censusdata_bin.rds")
+names(censusdata_bin) <- c("acs5_2010_bin","acs5_2015_bin","acs5_2019_bin")
+acs10<-as.matrix(censusdata_bin$acs5_2010_bin)
+Kmax<-c(10,20)
+alpha<- c(1, 1/(seq(20,60, by = 15)))
+m<-1000
+burnin<-500
+nChains<-4
+heats <- seq(1, 0.4, length = nChains)
+wd<-"/n/home03/crodriguezcabrera/BayesBinMix_Project/"
+
+Gammas<-c()
+Kmap<-c()
+for (k in Kmax){
+  for (g in alpha){
+  out.path<-paste0(wd, "acs10", sep= "_",k ,sep = "_",g)
+    gammavec<-rep(g, k)
+    res<- coupledMetropolis(Kmax=k, nChains = nChains, 
+                            heats = heats,
+                            binaryData = acs10, 
+                            outPrefix = out.path,
+                            m = m,
+                            gamma = gammavec, burn = burnin)
+    
+    #Collect Kmap and g
+    Gammas<-c(Gammas, g)
+    Kmap<-c(Kmap,Mode(res$K.mcmc)[1])
+  }
+}
+
+Mat<-matrix(nrow = 8, ncol = 3)
+colnames(Mat)<-c("Kmax", "Gamma", "Kmap")
+Mat[,1]<-c(rep(10,4), rep(20,4))
+Mat[,2]<-Gammas
+Mat[,3]<-Kmap
+
+
+plot<-Mat %>% as.data.frame()%>% ggplot(aes(x= Gamma, y = Kmap, color= as.factor(Kmax))) +
+ geom_line()
+
+
+save(Mat, plot, file = "test.RData")
+
+#load("test.RData")
