056cfbb92dd92076dddc3ad70ffcd25ac1fdf246
Carmen Rodriguez <crodriguezcabrera@g.harvard.edu>
2024-06-12 16:16:28 -0400

    new models and sensitivity prior

--- my_basic_job.sh
+++ my_basic_job.sh
@@ -3,9 +3,9 @@
 #SBATCH -c 5  
 #SBATCH -p shared 
 #SBATCH --mem=64000 
-#SBATCH -t 0-40:00:00 
-#SBATCH -o myoutput_binmodother_6.10_%j.out 
-#SBATCH -e myerrors_binmodother_6.10_%j.err  
+#SBATCH -t 0-20:00:00 
+#SBATCH -o myoutput_binmodother_6.11_%j.out 
+#SBATCH -e myerrors_binmodother_6.11_%j.err  
 #SBATCH --mail-type=BEGIN,END,FAIL  # Mail notifications
 #SBATCH --mail-user=crodriguezcabrera@g.harvard.edu   # Account to email
 
