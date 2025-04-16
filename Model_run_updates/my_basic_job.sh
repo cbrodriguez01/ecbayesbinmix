d7a03c5f66e5ece06c979261619f8b194a5e1c32
cbrodriguez01 <crodriguezcabrera@g.harvard.edu>
2025-04-16 19:31:33 -0400

    changes made in April 2025 (14 variables)

---
+++ Model_run_updates/my_basic_job.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH -c 5  
+#SBATCH -p shared 
+#SBATCH --mem=64000 
+#SBATCH -t 0-35:00:00 
+#SBATCH -o myoutput_updatedmods14v_4.16_%j.out 
+#SBATCH -e myerrors_updatedmods14v_4.16_%j.err  
+#SBATCH --mail-type=BEGIN,END,FAIL  # Mail notifications
+#SBATCH --mail-user=crodriguezcabrera@g.harvard.edu   # Account to email
+
+
+#Load R and the environment
+module load R/4.3.1-fasrc01
+export R_LIBS_USER=$HOME/apps/R_4.3.1:$R_LIBS_USER
+
+# Change the directory to where your scripts are
+cd $HOME/ecbayesbinmix/Model_run_updates/
+
+#Run the R script
+Rscript UpdatedModels_04162025.R
